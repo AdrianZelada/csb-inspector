9fb8f9d476cf6df663275e2f22d3b21c5f152085
adrian zelada torrez <zelada_torrez@hotmail.com>
2020-01-19 02:23:13 -0400

    Add Config Disabled browser, Changes Style browser

--- express-socket/index.js
+++ express-socket/index.js
@@ -14,16 +14,26 @@ module.exports = (options) => {
   const port = options.port || 8888;
   const host = getHost(ipHost, options.socket ? app.get('port') : port);
   router.get('/*', (req, res) => {
-    const dirHtml = path.join(clientApp, 'console.html');
-    fs.readFile(dirHtml, 'utf8',(err, data) => {
-        if (err) throw err;
-        data = data.replace('<title></title>',`<title>${configPack.name} - V${configPack.version}</title>`);
-        data = data.replace('_baseurl_', route + '/');
-        data = data.replace('<body>', `<body><span id="host" style="display: none;">${host}</span>`);
-        data = data.replace('</body>', `<span style="font-size: 10px; float:right">V${configPack.version}</span></body>`);
-        res.set('Content-Type', 'text/html');
-        res.send( Buffer.from(data));
-    });
+    if(!options.disabledBrowser){
+        const dirHtml = path.join(clientApp, 'console.html');
+        fs.readFile(dirHtml, 'utf8',(err, data) => {
+            if (err) throw err;
+            data = data.replace('<title></title>',`<title>${configPack.name} - V${configPack.version}</title>`);
+            data = data.replace('_baseurl_', route + '/');
+            data = data.replace('<body>', `<body><span id="host" style="display: none;">${host}</span>`);
+            data = data.replace('</body>', `<span style="font-size: 10px; float:right; color: white;">V${configPack.version}</span></body>`);
+            res.set('Content-Type', 'text/html');
+            res.send( Buffer.from(data));
+        });
+    } else {
+        const dirHtml = path.join(clientApp, 'assets/disabled.html');
+        fs.readFile(dirHtml, 'utf8',(err, data) => {
+            if (err) throw err;
+            res.set('Content-Type', 'text/html');
+            res.send( Buffer.from(data));
+        });
+    }
+
   });
 
   function LogErrors(err, req, res, next) {
@@ -36,10 +46,11 @@ module.exports = (options) => {
   app.use(route, express.static(clientApp));
   app.use(route, router);
   app.use(LogErrors);
-  const bus = connectionSocket(options.socket, port);
-
-  let channels = [bus.emitChannel];
-
+  let channels = [];
+  if(!options.disabledBrowser) {
+    const bus = connectionSocket(options.socket, port);
+    channels = [bus.emitChannel];
+  }
   if (options.outputs) {
     channels = [
       ...channels,
